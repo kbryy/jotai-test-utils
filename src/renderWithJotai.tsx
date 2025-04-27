3945f03c8d02c8dd660ca1efcd54b9557abcfdce
kbryy <kbry2124@gmail.com>
2025-04-27 15:09:48 +0900

    ビルド設定を修正

--- src/renderWithJotai.tsx
+++ src/renderWithJotai.tsx
@@ -1,12 +1,17 @@
 import { render } from "@testing-library/react";
 import { Provider } from "jotai";
-import type { ReactElement } from "react";
 import { createJotaiStore, type InitialValues } from ".";
+import type { RenderResult } from "@testing-library/react";
+import type { ReactElement } from "react";
+
+type RenderWithJotaiResult = RenderResult & {
+  store: ReturnType<typeof createJotaiStore>;
+};
 
 export const renderWithJotai = (
   ui: ReactElement,
   options: { initialValues?: InitialValues } = {},
-) => {
+): RenderWithJotaiResult => {
   const store = createJotaiStore(options.initialValues ?? []);
   const result = render(<Provider store={store}>{ui}</Provider>);
   return { ...result, store };
